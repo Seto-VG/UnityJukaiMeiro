18ac167fccae0ccc5a2c13ef5bcf8ecbb474be82
harada1152 <HUMANayu12615mu@gmail.com>
2023-07-05 16:11:52 +0900

    鍵、ドア完成版

--- MeiroProject/Assets/MeiroProject/Key&BlockScript SampleScene/BlockScript.cs
+++ MeiroProject/Assets/MeiroProject/Key&BlockScript SampleScene/BlockScript.cs
@@ -6,6 +6,7 @@ public class BlockScript : MonoBehaviour
     {
         Destroy(gameObject);
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") || other.CompareTag("Key"))
--- MeiroProject/Assets/MeiroProject/Key&BlockScript SampleScene/KeyScript.cs
+++ MeiroProject/Assets/MeiroProject/Key&BlockScript SampleScene/KeyScript.cs
@@ -2,11 +2,22 @@ using UnityEngine;
 
 public class KeyScript : MonoBehaviour
 {
+    public GameObject door_1; 
+    public GameObject door_2_Right;
+    public GameObject door_2_Left;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Destroy(gameObject); // キーオブジェクトを削除する
+            Destroy(gameObject);
+            Destroy(door_1);
+            Destroy(door_2_Right);
+            Destroy(door_2_Left);
+
         }
     }
+    
+
+    
 }
